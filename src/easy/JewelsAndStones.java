14de87b7bc25ae684cc08301898ed2a3c5eab51e
William Goins <de777urest@gmail.com>
2018-05-24 22:18:28 -0500

    2 Easy Solutions

---
+++ src/easy/JewelsAndStones.java
@@ -0,0 +1,38 @@
+package easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JewelsAndStones {
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
+        
+        for (char jewel : J.toCharArray()) {
+            for (char stone : S.toCharArray()) {
+                if (jewel == stone) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String J = line.toString();
+            line = in.readLine();
+            String S = line.toString();
+            
+            int ret = new JewelsAndStones().numJewelsInStones(J, S);
+            
+            String out = String.valueOf(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
+
---
+++ src/easy/UniqueMorseCodeWords.java
@@ -0,0 +1,48 @@
+package easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueMorseCodeWords {
+    public static final int ALPHABET_SIZE = 26;
+    public static final String morseAlphabet[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    
+    public int uniqueMorseRepresentations(String[] words) {
+        Set<String> transformations = new HashSet<String>(); 
+        
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            
+            for (char c : word.toCharArray()) {
+                sb.append(morseAlphabet[(int)c - 'a']);
+            }
+            
+            transformations.add(sb.toString());
+        }
+        
+        
+        return transformations.size();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            
+            
+            
+//            int ret = new UniqueMorseCodeWords().uniqueMorseRepresentations(words);
+            
+//            String out = String.valueOf(ret);
+            
+//            System.out.print(out);
+        }
+    }
+    
+    public static String[] stringToStringArray(String input) {
+        return input.split("\\r?\\n");
+    }
+}
